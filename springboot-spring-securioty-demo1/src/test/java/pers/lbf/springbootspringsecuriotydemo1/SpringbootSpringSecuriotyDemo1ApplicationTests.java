5b7b9bc937757acae0910bef37b3d72fbe7a31f7
赖柄沣 <laibingf_dev@outlook.com>
2020-08-27 17:20:25 +0800

    null

---
+++ springboot-spring-securioty-demo1/src/test/java/pers/lbf/springbootspringsecuriotydemo1/SpringbootSpringSecuriotyDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.lbf.springbootspringsecuriotydemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringSecuriotyDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
